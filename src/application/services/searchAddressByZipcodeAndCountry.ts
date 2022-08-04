84cab8bd278de6cda219cf8e2575ca31539c2e77
Jonathan Rocha <john.rocha@q4inc.com>
2022-08-04 19:43:03 -0300

    feat: add services for application layer

---
+++ src/application/services/searchAddressByZipcodeAndCountry.ts
@@ -0,0 +1,23 @@
+import { HttpGetClient, HttpStatusCode } from '@/application/contracts/http'
+import { modelToEntityAddressMapper } from '@/application/mappers'
+import { AddressModel } from '@/application/models'
+import { Address } from '@/domain/entities'
+import { UnexpectedError } from '@/domain/errors'
+import { SearchAddressByZipcodeAndCountry } from '@/domain/usecases'
+
+export class SearchAddressByZipcodeAndCountryService implements SearchAddressByZipcodeAndCountry {
+  constructor(private readonly httpGetClient: HttpGetClient<never, AddressModel>) {}
+
+  async search(zipCode: string, country: string): Promise<Address | undefined> {
+    const response = await this.httpGetClient.get({
+      url: `https://api.zippopotam.us/${country.toLowerCase()}/${zipCode}`
+    })
+
+    switch (response.statusCode) {
+      case HttpStatusCode.ok:
+        return modelToEntityAddressMapper(response.body)
+      default:
+        throw new UnexpectedError()
+    }
+  }
+}
