20a66841dc6303d8b33944cb340038275b0a1df6
Jonathan Rocha <john.rocha@q4inc.com>
2022-08-04 18:49:10 -0300

    feat: add Address domain entity

---
+++ src/domain/entities/address.ts
@@ -0,0 +1,10 @@
+export type Address = {
+  postCode: string
+  country: string
+  countryCode: string
+  placeName: string
+  longitude: string
+  latitude: string
+  state: string
+  stateCode: string
+}
